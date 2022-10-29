de7c879ba43bfb3e087d7aa63cbd96be0072f19d
Phomphiriya <63010648@kmitl.ac.th>
2022-10-30 02:03:08 +0700

    :hammer: fix diamond

--- tutor/homework/daimond.py
+++ tutor/homework/daimond.py
@@ -1,9 +1,19 @@
 inp = int(input("Enter your number: "))
-x = inp-1
-for i in range(inp-1):
-    for j in range(x):
-        print(" ",end='')
-    x = x-1 
-    for k in range(2*i+1):
+n = inp
+i = inp-3
+for row in range(inp):
+    print(" "*i,end="")
+    i -= 1
+    for column in range(row+1):
         print("*",end="")
     print("\n")
+# x = inp-1
+# for i in range(inp-1):
+#     for j in range(x):
+#         print(" ",end='')
+#     x = x-1 
+#     for k in range(2*i+1):
+#         print("*",end="")
+#     print("\n")
+
+
