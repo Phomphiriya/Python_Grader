46327d0119c335eb95694ea719eb17114876620c
Phomphiriya <63010648@kmitl.ac.th>
2022-11-04 01:52:33 +0700

    :hammer: grade finish

--- tutor/homework/grade.py
+++ tutor/homework/grade.py
@@ -2,19 +2,30 @@ inp_stu = int(input("Enter number of student : "))
 count = 0
 ls_score = []
 ls_check = []
+perfect_count = 0
+grade_count = 0
 
 while count < inp_stu:
     inp_score = input("Enter score : ")
     ls_score.append(int(inp_score))
     count += 1
+print("*********************")
 print(ls_score)
 for i in ls_score:
     # print(i)
     # print(type(i))
     if i >= 85:
         ls_check.append("Pefect")
+        perfect_count += 1
+        grade_count += 1
     elif i >= 60:
         ls_check.append("Pass")
+        grade_count += 1
     else:
         ls_check.append("Not Pass")
+        grade_count += 1
 print(ls_check)
+# print(perfect_count)
+# print(grade_count)
+print("The Perfect Score to All Score is :",round(perfect_count/grade_count,2))
+
