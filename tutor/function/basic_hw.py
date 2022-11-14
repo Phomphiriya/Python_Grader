bc90682292e821554a926f3bc116a898dfbe2ad6
Phomphiriya <63010648@kmitl.ac.th>
2022-11-14 23:28:06 +0700

    :hammer: Add basic_hw

---
+++ tutor/function/basic_hw.py
@@ -0,0 +1,11 @@
+## 1.Highlight the word (Highlight คำที่อยู่หลัง , โดยให้ตัวนั้นเป็นตัว Upper)
+## Input: hi my name is boom,boom
+## Output: hi my name is BOOM
+## Input: corn it is a corn,corn
+## Output: CORN it is a CORN
+
+
+
+## 2.Check vowel (Make by Function)
+## Input: Non live in Thailand and Non love Thailand
+## Output: oieiaiaaooaia
