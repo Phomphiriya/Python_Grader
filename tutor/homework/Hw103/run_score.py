5417bff34d370aa9d7e869eff4fd7c44fbe7330b
Phomphiriya <63010648@kmitl.ac.th>
2022-11-21 01:24:53 +0700

    :hammer: Do ans Hw103 and make example

--- tutor/homework/Hw103/run_score.py
+++ tutor/homework/Hw103/run_score.py
@@ -3,3 +3,12 @@
 # คะแนนที่ดีที่สุดแล้ว output ออกมา เช่น
 # input จำนวนนักเรียน n จากนั้นตั้งแต่ข้อมูลที่ 2 ไปจนถึง n+1 คือคะแนนของนักเรียน n
 # output คะแนนที่ดีที่สุด
+
+mem = int(input("Enter number of students : "))
+score = []
+
+for i in range(1,mem+1):
+    mem_score = int(input("Enter {} score of student : ".format(i)))
+    score.append(mem_score)
+# print(score)
+print("The Highest score is",max(score))
